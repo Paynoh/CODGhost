51f87e83540ce751baf2aa01e91061e2627df125
Paynoh <64705854+Paynoh@users.noreply.github.com>
2024-07-11 17:47:29 +0200

    Update dllmain.cpp

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,6 +1,45 @@
 #include <iostream>
 #include <Windows.h>
+/*class CWeapon
+{
+public:
+	char pad_0000[8]; //0x0000
+	uint32_t N0000070D; //0x0008
+	uint32_t CurrentWeapon; //0x000C
+	char pad_0010[8]; //0x0010
+	float CrossAir; //0x0018
+	char pad_001C[156]; //0x001C
+	uint32_t N00000723; //0x00B8
+	char pad_00BC[20]; //0x00BC
+	uint32_t Grenades; //0x00D0
+	char pad_00D4[3028]; //0x00D4
+	char *ShaderName; //0x0CA8
+	char pad_0CB0[1112]; //0x0CB0
+}; //Size: 0x1108
+static_assert(sizeof(CWeapon) == 0x1108);
 
+class Player
+{
+public:
+	char pad_0000[32]; //0x0000
+	float PlayerPositionX; //0x0020
+	float PlayerPositionY; //0x0024
+CameraAcceleration; //0x0028
+	char pad_0034[188]; //0x0034
+	float CameraPitch; //0x00F0
+	float CameraYaw; //0x00F4
+	char pad_00F8[804]; //0x00F8
+	class CWeapon m_Weapon; //0x041C
+	char pad_1524[4184]; //0x1524
+}; //Size: 0x257C
+static_assert(sizeof(Player) == 0x257C);
+
+class N000003F8
+{
+public:
+	char pad_0000[6280]; //0x0000
+}; //Size: 0x1888
+static_assert(sizeof(N000003F8) == 0x1888);*/
 
 using GetStringFromScriptingEnginefn = char* (__fastcall*)(const char* a1);
 
